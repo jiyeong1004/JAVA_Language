09495ca5197610c4e7386e4543fa6815f1316dfd
jiyeong1004 <ak1157@naver.com>
2021-05-11 23:34:49 +0900

    #2 2-1. 기본 자료형

---
+++ CHAPTER02/src/VarUse.java
@@ -0,0 +1,14 @@
+
+public class VarUse {
+
+	public static void main(String[] args) {
+		int k1 = 10;
+		int k2 = 20;
+		double d = 3.14;
+		char c = 'A';
+		
+		System.out.println("정수 데이터 k1 = " + k1 + ", k2 = " + k2);
+		System.out.println("실수 데이터 d = " + d);
+		System.out.println("문자 데이터 c = " + c);
+	}
+}
